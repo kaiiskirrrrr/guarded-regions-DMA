61ef4308f1e6fdb4f73c7ef9267c661b786d096b
Kaii Bala <168567851+kaiiskirrrrr@users.noreply.github.com>
2024-06-28 09:37:12 +1000

    Update guarded.h

--- guarded.h
+++ guarded.h
@@ -1,28 +1,26 @@
-	inline static bool isGuarded(uintptr_t pointer) noexcept
+	uintptr_t guard_address = 0;	
+
+        inline static auto is_guarded(uintptr_t pointer) -> bool noexcept
 	{
 		static constexpr uintptr_t filter = 0xFFFFFFF000000000;
 		uintptr_t result = pointer & filter;
 		return result == 0x8000000000 || result == 0x10000000000;
 	}
 
-	uintptr_t guard_address = 0;
-
-	inline uintptr_t validatePointer(uintptr_t address) {
-		return isGuarded(address) ? guard_address + (address & 0xFFFFFF) : address;
+	inline auto validate_pointer(uintptr_t address) -> uintptr_t {
+		return is_guarded(address) ? guard_address + (address & 0xFFFFFF) : address;
 	}
 
-	inline bool isKernel(uintptr_t ptr) {
+	inline auto is_kernal(uintptr_t ptr) -> bool {
 		return (ptr & 0xFFF0000000000000) == 0xFFF0000000000000;
 	}
 
-	uintptr_t Read2(uintptr_t address, bool cache = false) {
+	inline uintptr_t Read2(uintptr_t address, bool cache = false) {
 		uintptr_t buffer{};
 		int pid = 4;
 
-		if (!isKernel(address))
+		if (!is_kernal(address))
 			pid = processInfo.pid;
-
-		//std::cout << "read from pid: " << std::to_string(pid).c_str() << std::endl;
 
 		auto flags = VMMDLL_FLAG_NOCACHE | VMMDLL_FLAG_NOPAGING | VMMDLL_FLAG_NOCACHEPUT | VMMDLL_FLAG_NOPAGING_IO;
 		if (cache) {
@@ -32,10 +30,10 @@
 
 		VMMDLL_MemReadEx(DMA_HANDLE, pid, (ULONG64)address, reinterpret_cast<PBYTE>(&buffer), sizeof(buffer), NULL, flags);
 
-		return validatePointer(buffer);
+		return validate_pointer(buffer);
 	}
 
-	ULONG64 GetGuardedRegion()
+	ULONG64 get_guarded_region()
 	{
 		PVMMDLL_MAP_POOL pPool;
 
@@ -57,10 +55,4 @@
 
 		VMMDLL_MemFree(pPool);
 		return NULL;
-
-
-
-		//guard_address = Read2(vgk + 0x7FCE0);
-
-		//return guard_address;
 	}
