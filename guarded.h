7c8b6587c38c005694d76b8f40c4c7d0bf3385d8
Kaii Misa <168567851+kaiiskirrrrr@users.noreply.github.com>
2024-09-21 04:58:13 +1000

    Update guarded.h

--- guarded.h
+++ guarded.h
@@ -1,67 +1,60 @@
-	uintptr_t guard_address = 0;	
+class guarded
+{
+public:
+    uintptr_t guard_address = 0;
 
-        inline static auto is_guarded(uintptr_t pointer) -> bool noexcept
-	{
-		static constexpr uintptr_t filter = 0xFFFFFFF000000000;
-		uintptr_t result = pointer & filter;
-		return result == 0x8000000000 || result == 0x10000000000;
-	}
+    static auto is_guarded(const uintptr_t& pointer) noexcept -> bool
+    {
+        static constexpr uintptr_t filter = 0xFFFFFFF000000000;
+        return (pointer & filter) == 0x8000000000 or (pointer & filter) == 0x10000000000;
+    }
 
-	inline auto validate_pointer(uintptr_t address) -> uintptr_t {
-		return is_guarded(address) ? guard_address + (address & 0xFFFFFF) : address;
-	}
+    auto validate_pointer(const uintptr_t& address) -> uintptr_t
+    {
+        return is_guarded(address) ? guard_address + (address & 0xFFFFFF) : address;
+    }
 
-	inline auto is_kernal(uintptr_t ptr) -> bool {
-		return (ptr & 0xFFF0000000000000) == 0xFFF0000000000000;
-	}
+    auto is_kernal(const uintptr_t& ptr) -> bool
+    {
+        return (ptr & 0xFFF0000000000000) == 0xFFF0000000000000;
+    }
 
-	inline uintptr_t read_two(uintptr_t address, bool cache = false) {
-		uintptr_t buffer{};
-		int pid = 4;
+    auto read_two(uintptr_t address) -> uintptr_t
+    {
+        uintptr_t buffer{};
+        int pid = is_kernal(address) ? 4 : processInfo.pid;
 
-		if (!is_kernal(address))
-			pid = processInfo.pid;
+        auto flags = VMMDLL_FLAG_NOCACHE | VMMDLL_FLAG_NOPAGING | VMMDLL_FLAG_NOCACHEPUT | VMMDLL_FLAG_NOPAGING_IO;
+        VMMDLL_MemReadEx(DMA_HANDLE, pid, static_cast<ULONG64>(address), reinterpret_cast<PBYTE>(&buffer), sizeof(buffer), NULL, flags);
 
-		auto flags = VMMDLL_FLAG_NOCACHE | VMMDLL_FLAG_NOPAGING | VMMDLL_FLAG_NOCACHEPUT | VMMDLL_FLAG_NOPAGING_IO;
-		if (cache) {
-			flags &= ~VMMDLL_FLAG_NOCACHE;
-			flags &= ~VMMDLL_FLAG_NOCACHEPUT;
-		}
+        return validate_pointer(buffer);
+    }
 
-		VMMDLL_MemReadEx(DMA_HANDLE, pid, (ULONG64)address, reinterpret_cast<PBYTE>(&buffer), sizeof(buffer), NULL, flags);
+    auto create_scatter_handle(int pid) const -> VMMDLL_SCATTER_HANDLE
+    {
+        if (pid == 0) { pid = processInfo.pid; }
+        return VMMDLL_Scatter_Initialize(handle, pid, misa_flags);
+    }
 
-		return validate_pointer(buffer);
-	}
+    auto get_guarded_region() -> ULONG64
+    {
+        PVMMDLL_MAP_POOL pPool;
+        VMMDLL_Map_GetPool(DMA_HANDLE, &pPool, NULL);
 
-        inline VMMDLL_SCATTER_HANDLE create_scatter_handle(int pid) const
+        for (DWORD i = 0; i < pPool->cMap; ++i)
         {
+            PVMMDLL_MAP_POOLENTRY pEntry = &pPool->pMap[i];
 
-	if (pid == 0) pid = processInfo.pid;
-
-	const VMMDLL_SCATTER_HANDLE scatter_handle = VMMDLL_Scatter_Initialize(handle, pid, misa_flags);
-	return scatter_handle;
+            if (pEntry->cb == 0x200000 && memcmp(pEntry->szTag, "ConT", 4) == 0)
+            {
+                guard_address = pEntry->va;
+                VMMDLL_MemFree(pPool);
+                return guard_address;
+            }
         }
 
-	auto get_guarded_region() -> ULONG64
-	{
-		PVMMDLL_MAP_POOL pPool;
+        VMMDLL_MemFree(pPool);
+        return NULL;
+    }
 
-		VMMDLL_Map_GetPool(DMA_HANDLE, &pPool, NULL);
-
-		for (DWORD i = 0; i < pPool->cMap; ++i) {
-			PVMMDLL_MAP_POOLENTRY pEntry = &pPool->pMap[i];
-
-			if (pEntry->cb == 0x200000 && memcmp(pEntry->szTag, "ConT", 4) == 0) {
-
-				ULONG64 result = pEntry->va;
-
-				VMMDLL_MemFree(pPool);
-
-				guard_address = result;
-				return result;
-			}
-		}
-
-		VMMDLL_MemFree(pPool);
-		return NULL;
-	}
+}; inline const auto c_guarded = std::make_unique<guarded>();
